4bb9c0a947f8a28d49232bea5468c07d50708712
Tyler Hanavan <TylerHanavan@gmail.com>
2018-06-11 01:30:05 -0400

    added asserttrue method for unit testing

--- backend/test-suite/test.php
+++ backend/test-suite/test.php
@@ -1,5 +1,9 @@
 <?php
 
-  
+  function assertTrue($case, $message = 'A case was false!') {
+    if(!$case) $passed = false;
+    echo '<b>Test Case Failed for assertTrue: ',$message,'</b>';
+    exit();
+  }
 
  ?>

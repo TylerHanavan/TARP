215b81e6d413e2d767878fc09920868ef6152b71
Dewan's Islam <DewanIslam@Dewans-Macbook-Air.local>
2018-06-12 12:24:46 -0400

    submit.php form fields via post added

--- backend/submit/submit.php
+++ backend/submit/submit.php
@@ -1,5 +1,12 @@
 <?php
+// Filter input, Format it , Fill database
 
-	
+  $taName = $_POST['taName'];
+  $taDescription = $_POST["taDescription"];
+  $comments = $_POST["comments"];
+  $overallExperience = $_POST["overallExperience"];
+  $importance = $_POST["importance"];
+  $name = $_POST["name"];
+  $contactInfo = $_POST["contactInfo"];
 
 ?>

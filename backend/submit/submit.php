0a92c19fe1b822a9d007c5ef3cef6b27262cf63e
Dewan's Islam <DewanIslam@Dewans-Macbook-Air.local>
2018-06-12 12:35:25 -0400

    added includes for database to submit php file

--- backend/submit/submit.php
+++ backend/submit/submit.php
@@ -1,5 +1,7 @@
 <?php
 // Filter input, Format it , Fill database
+include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/database.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/credentials.php';
 
   $taName = $_POST['taName'];
   $taDescription = $_POST["taDescription"];

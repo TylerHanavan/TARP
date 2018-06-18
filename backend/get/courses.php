7474f685923c4c47cf9c1cf5a27e15ecb5fee264
Dewan's Islam <DewanIslam@Dewans-Macbook-Air.local>
2018-06-18 16:19:40 -0400

    #54 added json encoding for course retrival

--- backend/get/courses.php
+++ backend/get/courses.php
@@ -4,3 +4,6 @@ include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/
 
 $database = new Database($CREDENTIALS["address"],$CREDENTIALS["database"], $CREDENTIALS["username"], $CREDENTIALS["password"]);
 $database->connect();
+
+echo json_encode ($database->getCourses());
+exit();

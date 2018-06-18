eb51cd99f370b3ba69303d28204b3e8e77642095
Dewan's Islam <DewanIslam@Dewans-Macbook-Air.local>
2018-06-18 16:17:07 -0400

    #54 created new folder and file for retrival of course records

---
+++ backend/get/courses.php
@@ -0,0 +1,6 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/database.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/credentials.php';
+
+$database = new Database($CREDENTIALS["address"],$CREDENTIALS["database"], $CREDENTIALS["username"], $CREDENTIALS["password"]);
+$database->connect();

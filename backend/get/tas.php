15bfb2fd6ce357354a94c1579f6d474dca8c8e99
Tyler Hanavan <TylerHanavan@gmail.com>
2018-06-18 21:40:27 -0400

    #49 add tas.php file to the get folder for retrieval of data from tas table

---
+++ backend/get/tas.php
@@ -0,0 +1,5 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/database.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/credentials.php';
+
+exit();

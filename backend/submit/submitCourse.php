50ad23d43b7a34912cb153beac5fb24330634c79
Alex Chrysanthem <achrysan@buffalo.edu>
2018-06-19 16:03:42 -0400

    #65 created submitCourse.php, added form tag to modal window, should be able to create a class and have it show in the database

---
+++ backend/submit/submitCourse.php
@@ -0,0 +1,16 @@
+<?php
+// Filter input, Format it , Fill database
+include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/database.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/credentials.php';
+
+  $name = $_POST["name"];
+  $instructor = $_POST['instructor'];
+  
+
+  $database = new Database($CREDENTIALS["address"],$CREDENTIALS["database"], $CREDENTIALS["username"], $CREDENTIALS["password"]);
+  $database->connect();
+  $database->addCourse($name,$instructor);
+  //header('Location: ../../frontend/success.html?success=1');
+  exit();
+
+?>

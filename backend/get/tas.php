d16c8c92c4d2ea5c85b249bad685e9f28f652e5d
Dewan's Islam <DewanIslam@Dewans-Macbook-Air.local>
2018-06-19 11:20:31 -0400

    #63 allow parameters to be passed into tas.php in get folder to specify course

--- backend/get/tas.php
+++ backend/get/tas.php
@@ -1,9 +1,10 @@
 <?php
+$course = $_GET ['course'];
 include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/database.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/credentials.php';
 
 $database = new Database($CREDENTIALS["address"],$CREDENTIALS["database"], $CREDENTIALS["username"], $CREDENTIALS["password"]);
 $database->connect();
 
-echo json_encode ($database->getTAs());
+echo json_encode ($database->getTAs($course));
 exit();

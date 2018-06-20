564d7926c7a1b5b4778d22153dfba3a275091fd9
Dewan's Islam <DewanIslam@Dewans-Macbook-Air.local>
2018-06-20 15:38:14 -0400

    #66 check if course or ta is passed to feedback.php

--- backend/get/feedback.php
+++ backend/get/feedback.php
@@ -1,10 +1,12 @@
 <?php
 $course = $_GET ['course'];
+$ta = $_GET ['ta'];
 include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/database.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/credentials.php';
 
 $database = new Database($CREDENTIALS["address"],$CREDENTIALS["database"], $CREDENTIALS["username"], $CREDENTIALS["password"]);
 $database->connect();
-
-echo json_encode ($database->getFeedback($course));
+if(!empty($course) && isset($course)) {
+  echo json_encode ($database->getFeedback($course));
+}
 exit();

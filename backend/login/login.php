b4da54dcdc30b24b5f959edf27734207b90b36bd
TylerHanavan <TylerHanavan@gmail.com>
2018-06-21 20:37:22 -0400

    #90 added basic includes to database

--- backend/login/login.php
+++ backend/login/login.php
@@ -1,5 +1,6 @@
 <?php
 
-  
+  include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/database.php';
+  include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/credentials.php';
 
  ?>

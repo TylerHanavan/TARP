b342a3a041c06cca615aefc890b774caada3eacf
TylerHanavan <TylerHanavan@gmail.com>
2018-06-21 20:37:49 -0400

    #90 added code to allow connection to database

--- backend/login/login.php
+++ backend/login/login.php
@@ -3,4 +3,7 @@
   include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/database.php';
   include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/credentials.php';
 
+  $database = new Database($CREDENTIALS["address"],$CREDENTIALS["database"], $CREDENTIALS["username"], $CREDENTIALS["password"]);
+  $database->connect();
+
  ?>

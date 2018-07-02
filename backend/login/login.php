dae28f8d2db5dc7c3fd71c84aa77729c71d451b0
TylerHanavan <TylerHanavan@gmail.com>
2018-07-01 21:33:31 -0400

    #106 removed redundant database connection in login.php

--- backend/login/login.php
+++ backend/login/login.php
@@ -3,11 +3,7 @@
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
 
-  include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/credentials.php';
   include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/session/session.php';
-
-  $database = new Database($CREDENTIALS["address"],$CREDENTIALS["database"], $CREDENTIALS["username"], $CREDENTIALS["password"]);
-  $database->connect();
 
   $username = $_POST['username'];
   $password = $_POST['password'];

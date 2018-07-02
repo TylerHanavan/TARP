b20c4988c4c0a6271f2d23ced4507f1375670131
TylerHanavan <TylerHanavan@gmail.com>
2018-07-01 21:07:18 -0400

    #106 added basic database/credential includes, and database connection to session.php

--- backend/session/session.php
+++ backend/session/session.php
@@ -1,5 +1,11 @@
 <?php
 
+  include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/database.php';
+  include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/credentials.php';
+
+  $database = new Database($CREDENTIALS["address"],$CREDENTIALS["database"], $CREDENTIALS["username"], $CREDENTIALS["password"]);
+  $database->connect();
+
   
 
  ?>

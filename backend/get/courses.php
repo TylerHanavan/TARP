c01794edef27b70acdc127fd6657120d76c1b34f
TylerHanavan <TylerHanavan@gmail.com>
2018-07-01 21:33:58 -0400

    #106 removed redundant database connection in courses.php

--- backend/get/courses.php
+++ backend/get/courses.php
@@ -1,10 +1,6 @@
 <?php
 
-include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/database.php';
-include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/credentials.php';
+  include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/session.php';
 
-$database = new Database($CREDENTIALS["address"],$CREDENTIALS["database"], $CREDENTIALS["username"], $CREDENTIALS["password"]);
-$database->connect();
-
-echo json_encode ($database->getCourses());
-exit();
+  echo json_encode ($database->getCourses());
+  exit();

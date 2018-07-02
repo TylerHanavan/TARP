c48239be4cd2f5002b8c332f4ec43e68331b2ccf
TylerHanavan <TylerHanavan@gmail.com>
2018-07-01 21:22:08 -0400

    #106 include session.php in login.php

--- backend/login/login.php
+++ backend/login/login.php
@@ -2,6 +2,7 @@
 
   include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/database.php';
   include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/credentials.php';
+  include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/session/session.php';
 
   $database = new Database($CREDENTIALS["address"],$CREDENTIALS["database"], $CREDENTIALS["username"], $CREDENTIALS["password"]);
   $database->connect();

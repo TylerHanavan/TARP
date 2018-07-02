034d9603ed0642d44d2280be9c6ea7f22fbdbb2d
TylerHanavan <TylerHanavan@gmail.com>
2018-07-01 23:42:58 -0400

    #23 added a detail to user_details.php to return list of courses a user owns

--- backend/get/user_details.php
+++ backend/get/user_details.php
@@ -12,6 +12,12 @@
   } else {
     $data['username'] = $username;
     $data['user_id'] = $user_id;
+    $courses = $database->getCourses($user_id);
+    $course_string = '';
+    for($x = 0; $x < sizeof($courses); $x++) {
+      $course_string .= $courses[$x]['id'] . ',';
+    }
+    $data['courses'] = $course_string;
   }
 
 

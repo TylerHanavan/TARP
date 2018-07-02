fa5ea6a85b6aae648ec262df8cfdbead0a4812fd
TylerHanavan <TylerHanavan@gmail.com>
2018-07-01 21:16:21 -0400

    #106 added code to echo information if debug mode is on; addedcookie tracking and handling to session.php

--- backend/session/session.php
+++ backend/session/session.php
@@ -7,8 +7,11 @@
   $database->connect();
 
   $logged_in = false;
+  $debug = false;
 
   session_start();
+  
+  if($debug) var_dump(session_id());
 
   function getRandStr($size = 7, $caps_only = false) {
     $p = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
@@ -19,6 +22,18 @@
       $r .= $p[rand(0, strlen($p))];
     }
     return $r;
+  }
+
+  if(isset($_COOKIE['sess']) && !empty($_COOKIE['sess'])) {
+    $cook = $_COOKIE['sess'];
+  }
+
+  if(isset($cook) && !empty($cook)) {
+    if($debug) echo 'Sess was already set: ', $cook;
+  } else {
+    $cook = getRandStr();
+    setcookie('sess', $cook, time() + 24 * 60 * 60 * 3, '/');
+    if($debug) echo 'Session was not set: ', $cook;
   }
 
  ?>

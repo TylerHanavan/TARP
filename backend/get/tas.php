c1d980687d302c64eb2eb580489631d572891669
TylerHanavan <TylerHanavan@gmail.com>
2018-07-01 21:31:35 -0400

    #106 fixed login.php includes

--- backend/get/tas.php
+++ backend/get/tas.php
@@ -2,13 +2,9 @@
 $course = $_GET ['course'];
 include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/database.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/credentials.php';
-include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/session/session.php';
 
 $database = new Database($CREDENTIALS["address"],$CREDENTIALS["database"], $CREDENTIALS["username"], $CREDENTIALS["password"]);
 $database->connect();
 
-$data = $database->getTAsForCourse($course);
-$data['username'] = $username;
-
-echo json_encode ($data);
+echo json_encode ($database->getTAsForCourse($course));
 exit();
--- backend/login/login.php
+++ backend/login/login.php
@@ -1,6 +1,8 @@
 <?php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
 
-  include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/database.php';
   include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/credentials.php';
   include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/session/session.php';
 

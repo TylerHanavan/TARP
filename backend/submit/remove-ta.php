7e6769c2e96803046c4251d8f6fa0f8d8653e23b
Dewan's Islam <DewanIslam@Dewans-Macbook-Air.local>
2018-07-03 11:42:21 -0400

    #69 called removeTA function in remove-ta.php and changed post parameters to fit removeTA

--- backend/submit/remove-ta.php
+++ backend/submit/remove-ta.php
@@ -1,10 +1,10 @@
 <?php
 $course = $_POST['course'];
-$taName = $_POST["taName"];
+$id = $_POST["id"];
 include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/database.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/credentials.php';
 
 $database = new Database($CREDENTIALS["address"],$CREDENTIALS["database"], $CREDENTIALS["username"], $CREDENTIALS["password"]);
 $database->connect();
-$database->addTA($taName, $course);
+$database->removeTA($id, $course);
 exit();

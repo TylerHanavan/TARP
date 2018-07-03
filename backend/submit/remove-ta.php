36999ee2a8fbb5542c2122abe01dcc8b3d0960ce
Dewan's Islam <DewanIslam@Dewans-Macbook-Air.local>
2018-07-03 11:44:43 -0400

    #69 removed redundant database calls in remove-ta.php

--- backend/submit/remove-ta.php
+++ backend/submit/remove-ta.php
@@ -3,7 +3,5 @@ $course = $_POST['course'];
 $id = $_POST["id"];
 include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/session/session.php';
 
-$database = new Database($CREDENTIALS["address"],$CREDENTIALS["database"], $CREDENTIALS["username"], $CREDENTIALS["password"]);
-$database->connect();
 $database->removeTA($id, $course);
 exit();

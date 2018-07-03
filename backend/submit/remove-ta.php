7aefdd9f730d88e6bb5fd5f7d43871368650e2df
Dewan's Islam <DewanIslam@Dewans-Macbook-Air.local>
2018-07-03 12:41:32 -0400

    #69 fixed error preventing actual removal of ta in remove-ta.php

--- backend/submit/remove-ta.php
+++ backend/submit/remove-ta.php
@@ -3,11 +3,18 @@ $course = $_POST['course'];
 $id = $_POST["id"];
 include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/session/session.php';
 if (!$logged_in) {
+  echo 'not logged in';
   exit();
 }
+$b = false;
 $courses = $database->getCourses($user_id);
-if (!in_array($course, $courses)) {
+for($x = 0; $x < sizeof($courses); $x++)
+  if($courses[$x]['id'] == $course)
+    $b = true;
+if(!$b) {
+  echo 'not a course you own';
   exit();
 }
 $database->removeTA($id, $course);
+echo 'success';
 exit();

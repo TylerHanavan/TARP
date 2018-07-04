b31e1a13e8c6b8ee89c54c923e6240edd43b9021
TylerHanavan <TylerHanavan@gmail.com>
2018-07-04 01:24:32 -0400

    #19 added ability to specify a ta by url get parameter in tas.php

--- backend/get/tas.php
+++ backend/get/tas.php
@@ -1,5 +1,6 @@
 <?php
 $course = $_GET ['course'];
+$ta = $_GET ['ta'];
 include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/database.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/credentials.php';
 
@@ -7,6 +8,15 @@ $database = new Database($CREDENTIALS["address"],$CREDENTIALS["database"], $CRED
 $database->connect();
 
 $data = $database->getTAsForCourse($course);
+$send = array();
+
+if(isset($ta) && !empty($ta)) {
+  for($x = 0; $x < sizeof($data); $x++) {
+    if($data[$x]['ta'] == $ta) {
+      $send[$x] = $data[$x];
+    }
+  }
+}
 
 echo json_encode ($data);
 exit();

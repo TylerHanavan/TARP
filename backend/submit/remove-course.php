99a4198376115da6de961f68f1318beed6a01ec6
TylerHanavan <TylerHanavan@gmail.com>
2018-07-04 19:43:05 -0400

    #120 removed unnecessary debug messages

--- backend/submit/remove-course.php
+++ backend/submit/remove-course.php
@@ -4,7 +4,6 @@ $course = $_POST['course'];
 include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/session/session.php';
 
 if (!$logged_in) {
-  echo 'please login';
   header('Location: ../../frontend/course.html?course=' . $course);
   exit();
 }
@@ -15,7 +14,6 @@ for($x = 0; $x < sizeof($courses); $x++)
   if($courses[$x]['id'] == $course)
     $b = true;
 if(!$b) {
-  echo 'please don\'t delete courses you don\'t own';
   header('Location: ../../frontend/course.html?course=' . $course);
   exit();
 }

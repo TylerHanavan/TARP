3478d071371976090349a82f28c45f492dc47b2d
TylerHanavan <TylerHanavan@gmail.com>
2018-07-04 01:00:50 -0400

    #19 modified tas.php to preserve ta name after deletion

--- backend/get/tas.php
+++ backend/get/tas.php
@@ -6,5 +6,13 @@ include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/
 $database = new Database($CREDENTIALS["address"],$CREDENTIALS["database"], $CREDENTIALS["username"], $CREDENTIALS["password"]);
 $database->connect();
 
-echo json_encode ($database->getTAsForCourse($course));
+$data = $database->getTAsForCourse($course);
+
+for($x = 0; $x < sizeof($data); $x++) {
+  if($data[$x]['deleted'] == 1) {
+    $data[$x]['name'] = $data[$x]['name'] . ' (deleted)';
+  }
+}
+
+echo json_encode ($data);
 exit();

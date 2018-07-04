281ecb304944fa331354b84d6d8d487de5a720c7
TylerHanavan <TylerHanavan@gmail.com>
2018-07-04 18:55:22 -0400

    #120 prevent adding tas if the user isn't logged in

--- backend/submit/add-ta.php
+++ backend/submit/add-ta.php
@@ -3,5 +3,10 @@ $course = $_POST['course'];
 $taName = $_POST["taName"];
 include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/session/session.php';
 
+if (!$logged_in) {
+  header('Location: ../../frontend/course.html?course=' . $id);
+  exit();
+}
+
 $database->addTA($taName, $course);
 exit();

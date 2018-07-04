45e57e3115cb3ddc16c5e975487d7105f9e6fd95
TylerHanavan <TylerHanavan@gmail.com>
2018-07-04 18:56:38 -0400

    #120 modified php includes in remove-course.php and removed redundant database connections

--- backend/submit/remove-course.php
+++ backend/submit/remove-course.php
@@ -1,10 +1,8 @@
 <?php
 $course = $_POST['course'];
-include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/database.php';
-include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/sql/credentials.php';
 
-$database = new Database($CREDENTIALS["address"],$CREDENTIALS["database"], $CREDENTIALS["username"], $CREDENTIALS["password"]);
-$database->connect();
+include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/session/session.php';
+
 $database->removeCourse($course);
 header("Location: ../../frontend/index.html");
 exit();

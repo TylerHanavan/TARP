c9738330f93245a10a14df5f445b3227741f195c
TylerHanavan <TylerHanavan@gmail.com>
2018-07-04 19:46:17 -0400

    #120 removed debugging upon page error

--- backend/submit/submitCourse.php
+++ backend/submit/submitCourse.php
@@ -13,9 +13,9 @@
 
   $tas = explode(',', $taName);
 
-  for($x = 0; $x < sizeof($tas); $x++) {
-	$database->addTA($tas[$x], $id['id']); //make courseID the second paramter here. rn its hard coded as always to CSE331(which is id 1 in db)
-  }
+  for($x = 0; $x < sizeof($tas); $x++)
+    if($tas[$x] != '' && isset($tas[$x]) && !empty($tas[$x]) && $tas[$x] != null)
+	     $database->addTA($tas[$x], $id['id']); //make courseID the second paramter here. rn its hard coded as always to CSE331(which is id 1 in db)
 
   //$courseID = get the id of the most recent course created;
   header('Location: ../../frontend/index.html');

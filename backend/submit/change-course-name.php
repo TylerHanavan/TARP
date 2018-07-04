85f4ce296b452274e06d7be0480fbe91adc4b1d6
TylerHanavan <TylerHanavan@gmail.com>
2018-07-04 02:13:22 -0400

    #97 fixed post parameter mismatch issue in course.html

--- backend/submit/change-course-name.php
+++ backend/submit/change-course-name.php
@@ -1,20 +1,20 @@
 <?php
-$name = $_POST['courseName'];
-$id = $_POST["editCourseId"];
+$name = $_POST['course'];
+$id = $_POST["courseId"];
 include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/session/session.php';
 if (!$logged_in) {
-  header('Location: ../../frontend/course.html?course=' . $course);
+  header('Location: ../../frontend/course.html?course=' . $id);
   exit();
 }
 $b = false;
 $courses = $database->getCourses($user_id);
 for($x = 0; $x < sizeof($courses); $x++)
-  if($courses[$x]['id'] == $course)
+  if($courses[$x]['id'] == $id)
     $b = true;
 if(!$b) {
-  header('Location: ../../frontend/course.html?course=' . $course);
+  header('Location: ../../frontend/course.html?course=' . $id);
   exit();
 }
 $database->changeCourseName($id, $name);
-header('Location: ../../frontend/course.html?course=' . $course);
+header('Location: ../../frontend/course.html?course=' . $id);
 exit();

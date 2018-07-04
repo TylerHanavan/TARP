b8f302024d4d6bc2c253a6f8655d9b915b55c8af
TylerHanavan <TylerHanavan@gmail.com>
2018-07-04 18:50:44 -0400

    #118 remove error reporting from login.php

--- backend/login/login.php
+++ backend/login/login.php
@@ -1,7 +1,4 @@
 <?php
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
 
   include $_SERVER['DOCUMENT_ROOT'] . '/CSE442-542/2018-Summer/team03/backend/session/session.php';
 
